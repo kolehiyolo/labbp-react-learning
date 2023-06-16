ef61ed4538d30f1561a147bfa887ae0ec542bbab
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-16 14:31:34 +0800

    Starter Files Ready
    1. Fetched starter files from the codesandbox repo

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <h1>Hello World!</h1>
 );
+
+//1. Apply CSS styles to App.jsx component
+//to match the appearance on the completed app:
+//https://c6fkx.csb.app/
+//2. Extract the contact card as a reusable Card component.
+//3. Use props to render the default Beyonce contact card
+//so the Card component can be reused for other contacts.
+//4. Import the contacts.js file to create card components.
