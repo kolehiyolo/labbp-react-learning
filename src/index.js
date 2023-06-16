41f4aaa558c1c8bc90cb13fb77d2c31580c4c577
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-16 21:09:23 +0800

    Reviewed Several ES6 Functions and Setup Challenge
    1. Revisited ES6 methods: Map, Filter, Reduce, Find, and FindIndex
    2. I've done this before with freeCodeCamp, but having Angela re-explain concepts in her own twisted genius little way is awesome as usual as it brings in a much needed perspective of "how exactly do we use these and why would we", as I always found having so many of these array manipulating methods be...unnecessary??? when I know I can do what they do myself through vanilla JS anyways
    3. Angela helped hammer them in better in my little braino and I look forward to actually find use for these bad bois
    4. Another fun part of this lesson is me trying to recreate the methods using just forEach
    5. I can even do this with a for loop, basically just to prove a point lol, but yeah it was a fun exercise and made me long for CodeWars. Might check out later
    6. Anyways, I've setup the instructions for the challenge of the lesson, and I'll go ahead and do that. Peace

--- src/index.js
+++ src/index.js
@@ -1,19 +1,95 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// var numbers = [3, 56, 2, 48, 5];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <h1>Hello World!</h1>
-);
+// * Map -Create a new array by doing something with each item in an array.
 
-var numbers = [3, 56, 2, 48, 5];
+// function doubleMap(x) {
+//   return x * 2;
+// }
+// const newNumbersMap = numbers.map(doubleMap);
+// console.log(numbers.map(doubleMap)); 
+// console.log(`newNumbersMap:`); 
+// console.log(newNumbersMap);
 
-//Map -Create a new array by doing something with each item in an array.
+// let newNumbersForEach = [];
+// function doubleForEach(x) {
+//   newNumbersForEach.push(x*2);
+// }
+// numbers.forEach(doubleForEach);
+// console.log(`newNumbersForEach:`); 
+// console.log(newNumbersForEach); 
 
-//Filter - Create a new array by keeping the items that return true.
+// * Filter - Create a new array by keeping the items that return true.
 
-//Reduce - Accumulate a value by doing something to each item in an array.
+// function findGreaterThanTen(num) {
+//   return num > 10
+// }
+// const newNumbersFilter = numbers.filter(findGreaterThanTen);
+// console.log(newNumbersFilter); 
 
-//Find - find the first item that matches from an array.
+// const newNumbersForEach = [];
+// function findGreaterThanTen(num) {
+//   if (num > 10) {
+//     newNumbersForEach.push(num);
+//   }
+// }
+// numbers.forEach(findGreaterThanTen);
+// console.log(newNumbersForEach);
 
-//FindIndex - find the index of the first item that matches.
+// * Reduce - Accumulate a value by doing something to each item in an array.
+
+// let numbersSum = 0;
+// function addNumToSum(num) {
+//   numbersSum += num;
+// };
+// numbers.forEach(addNumToSum);
+// console.log(numbersSum); 
+
+// function addNumToSum(numbersSum, currentNum) {
+//   return numbersSum + currentNum;
+// };
+// const numbersSum = numbers.reduce(addNumToSum);
+// console.log(numbersSum); 
+
+// * Find - find the first item that matches from an array.
+
+// function findGreaterThanTen(num) {
+//   return num > 10;
+// }
+// const answer = numbers.find(findGreaterThanTen);
+// console.log(answer); 
+
+// let answer = null;
+// function findGreaterThanTen(num) {
+//   if (num > 10 && answer === null) {
+//     answer = num;
+//   }
+// }
+// numbers.forEach(findGreaterThanTen);
+// console.log(answer); 
+
+// * FindIndex - find the index of the first item that matches.
+
+// function findGreaterThanTen(num) {
+//   return num > 10;
+// }
+// const answer = numbers.findIndex(findGreaterThanTen);
+// console.log(answer); 
+
+// console.log(numbers.find((num) => num > 10)); 
+
+// let answer = null;
+// function findGreaterThanTen(num, index) {
+//   if (num > 10 && answer === null) {
+//     answer = index;
+//   }
+// }
+// numbers.forEach(findGreaterThanTen);
+// console.log(answer); 
+
+// * Challenge
+// Import emojipedia
+// Create a new array that just has the meaning text from each entry
+// Each meaning text should be truncated so that it only has 100 characters
+// Use substring()
+
+
