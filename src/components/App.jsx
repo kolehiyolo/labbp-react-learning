37491e9c8988d49ddba016bcdc5ddeedb891ddea
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-16 16:14:40 +0800

    Mapping Lesson Done
    1. Learned more about mapping
    2. I already know this as I've done my own mapping from before, but Angela's way of explaining things really made me rethink these new ES6 methods
    3. I never looked at the inner functions inside these methods as if they're callback functions themselves, and Angela separating the map call from the actual inner callback function shone a light to this little fact

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import Card from "./Card.jsx";
 import contacts from "../contacts.js";
 
+function createCard(contact) {
+  return (
+    <Card 
+      key={contact.id}
+      id={contact.id}
+      name={contact.name}
+      imgURL={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      {contacts.map((contact, index) => {
-        return (
-          <Card 
-            key={'card-'+index}
-            name={contact.name}
-            imgURL={contact.imgURL}
-            phone={contact.phone}
-            email={contact.email}
-          />
-        );
-      })}
+      {contacts.map(createCard)}
     </div>
   );
 }
