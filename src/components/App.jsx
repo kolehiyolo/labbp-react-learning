1fbe95909c644b5cd5aa5192aacf838f27c258df
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-16 21:38:43 +0800

    Donezo
    1. I already know and use arrow functions frequently so this was a definite breeze
    2. Turned all functions under index.js to arrow functions
    3. Turned the mapping function in App.jsx to another arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,23 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
-function createEntry(emojiTerm) {
-  return (
-    <Entry
-      key={emojiTerm.id}
-      emoji={emojiTerm.emoji}
-      name={emojiTerm.name}
-      description={emojiTerm.meaning}
-    />
-  );
-}
-
 function App() {
   return (
     <div>
       <h1>
         <span>emojipedia</span>
       </h1>
-      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+      <dl className="dictionary">{emojipedia.map(
+        emojiTerm => 
+          (
+            <Entry
+              key={emojiTerm.id}
+              emoji={emojiTerm.emoji}
+              name={emojiTerm.name}
+              description={emojiTerm.meaning}
+            />
+          )
+      )}</dl>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,31 +3,30 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render( < App / > );
 
-// var numbers = [3, 56, 2, 48, 5];
+// * Arrow Function Learning
 
-////Map -Create a new array by doing something with each item in an array.
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+var numbers = [3, 56, 2, 48, 5];
+// function square(x) {
+//   return x * x;
+// }
+// const newNumbers = numbers.map(square);
+// console.log(newNumbers); 
 
-//////Filter - Create a new array by keeping the items that return true.
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
+// console.log(numbers.map(x => x*x));
 
-//Reduce - Accumulate a value by doing something to each item in an array.
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//     return accumulator + currentNumber;
-// })
+// //Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map( x => x * 2);
+
+////Filter - Create a new array by keeping the items that return true.
+// const newNumbers = numbers.filter(num => num < 10);
+
+//// Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumber = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber);
 
 ////Find - find the first item that matches from an array.
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
+// const newNumber = numbers.find(num => num > 10);
 
-////FindIndex - find the index of the first item that matches.
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
+// //FindIndex - find the index of the first item that matches.
+// const newNumber = numbers.findIndex(num => num > 10);
