fd7a3e4bf36dbd39486905210b5de788909fa2a1
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-16 21:29:09 +0800

    Updated to Latest ReactDOM Rendering
    1. It is what it is

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // var numbers = [3, 56, 2, 48, 5];
 
