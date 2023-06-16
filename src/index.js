5d913729c8fda0c2c664ac5a89be5dbc69ceae7f
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-16 20:27:21 +0800

    Starter Files Ready
    1. Fetched Angela's starter files for the lesson

--- src/index.js
+++ src/index.js
@@ -5,3 +5,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <h1>Hello World!</h1>
 );
+
+var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+
+//Filter - Create a new array by keeping the items that return true.
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+
+//Find - find the first item that matches from an array.
+
+//FindIndex - find the index of the first item that matches.
