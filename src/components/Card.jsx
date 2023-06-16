493c32ab7d0272eb5bda0d3e386729a19fb6fc71
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-16 14:49:00 +0800

    Built Card Component
    1. As per the step instructions, I created a dedicated component for the Card element itself, which I put in a separate file (/src/components/Card.jsx)
    2. Updated App.jsx accordingly, imported the new component and rendered in
    3. Also, apparently I don't have the App.jsx running yet in the index.js so fixed that, removed the default h1 greeting
    4. I also tweaked VS Code a bit after noticing some oddities with my code
    	4.1. Just a lil tangent here
    	4.2. So basically, I noticed that the indentations on the App.jsx and the Card.jsx files are different
    		4.2.1. App.jsx's is tighter than Card.jsx's
    	4.3. I looked into it and it seems like Angela's starter code has the tabs tighter, and my Card.jsx is code I wrote/edited a bit so it goes to my default wider tabs
    	4.4. I did a lil googlin and found that VS Code has a setting that automatically adjusts a file's tabbing depending on the file's conventions itself
    	4.5. I'm not a fan of this and I need my code uniform, so I disabled this auto feature and set the tab width to 2 instead of the default 4 (I liked Angela's tighter tabs)
    	4.6. Of course, I beautified all code to reflect this new change

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="card">
+      <div className="top">
+        <h2>Beyonce</h2>
+        <img
+            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+            alt="avatar_img"
+        />
+        </div>
+      <div className="bottom">
+        <p>+123 456 789</p>
+        <p>b@beyonce.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
