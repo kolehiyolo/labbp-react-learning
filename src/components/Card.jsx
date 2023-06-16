9209d76d127091304b14757d1c8e1b5af4cc6ee1
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-16 16:01:13 +0800

    Further Breaking Down Components + React Dev Tools
    1. As instructed, I took the image and the bottom paragraphs and turned them into components
    2. Besides that, the main lesson is about the React Dev Tools, which is a surprisingly simple tool that really would've helped me when I was learning React, in that it simplifies the DOM to the bare essentials
    3. Will definitely be using it moving forward

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,17 @@
 import React from "react";
+import Avatar from "./Avatar.jsx";
+import Detail from "./Detail.jsx";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img
-            className="circle-img"
-            src={props.imgURL}
-            alt="avatar_img"
-        />
+        <Avatar imgURL={props.imgURL}/>
         </div>
       <div className="bottom">
-        <p className="info">{props.phone}</p>
-        <p className="info">{props.email}</p>
+        <Detail info={props.phone}/>
+        <Detail info={props.email}/>
       </div>
     </div>
   );
