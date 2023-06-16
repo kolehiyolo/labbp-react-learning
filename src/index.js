4d23056027251a675a989dea08706866f66d7cf4
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-16 14:29:11 +0800

    Further Minification
    1. Just removed the strictmode setting and the commented out app component from /src/index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,5 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
     <h1>Hello World!</h1>
-  </React.StrictMode>
 );
