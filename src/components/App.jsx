c2c1074192ca74f7e2eafaab0acd346e116b2a57
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-19 17:17:52 +0800

    Setup a Primitive Conditional Rendering Sample
    1. My solution is just one I based off of my understanding of React, aided by the very helpful principle of expressions vs statements
    2. I managed to make it work, and now Angela has some more challenges

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 
+let isLoggedIn = false;
+// let isLoggedIn = true;
+
 function App() {
+  function checkLoginThenRender() {
+    if (isLoggedIn) {
+      return (
+        <h1>Welcome</h1>
+      );
+    } else {
+      return (
+        <>
+          <h1>Hello</h1>
+          <form className="form">
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Login</button>
+          </form>
+        </>
+      );
+    }
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+      {checkLoginThenRender()}
     </div>
   );
 }
