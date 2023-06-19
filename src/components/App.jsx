2361e7892e0ea1ba46fff1fd5cca2a6c7d47ce14
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-19 17:39:01 +0800

    Tidied up further
    1. Just simplified the code more

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,44 +5,17 @@ let isLoggedIn = false;
 // let isLoggedIn = true;
 
 function App() {
-  function checkLoginThenRender() {
-    // if (isLoggedIn) {
-    //   return (
-    //     <h1>Welcome</h1>
-    //   );
-    // } else {
-    //   return (
-    //     <>
-    //       <h1>Hello</h1>
-    //       <Login />
-    //     </>
-    //   );
-    // }
-    return isLoggedIn
-      ?
-        <h1>Welcome</h1>
-      :
-        (
-          <>
-            <h1>Hello</h1>
-            <Login />
-          </>
-        );
-  }
-
   return (
     <div className="container">
-      {/* {checkLoginThenRender()} */}
       {isLoggedIn
-      ?
-        <h1>Welcome</h1>
-      :
-        (
-          <>
-            <h1>Hello</h1>
-            <Login />
-          </>
-        )}
+        ? <h1>Welcome</h1>
+        : (
+            <>
+              <h1>Hello</h1>
+              <Login />
+            </>
+          )
+      }
     </div>
   );
 }
