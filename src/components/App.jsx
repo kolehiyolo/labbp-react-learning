0c67b6eaf2021937b98f820ca92296b3a3f6cc56
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-19 18:07:33 +0800

    Removed Comments
    1. All good and managed to nail Angela's challenge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,9 +13,3 @@ function App() {
 }
 
 export default App;
-
-//Challenge: Without moving the userIsRegistered variable,
-//1. Show Login as the button text if userIsRegistered is true.
-//Show Register as the button text if userIsRegistered is false.
-//2. Only show the Confirm Password input if userIsRegistered is false.
-//Don't show it if userIsRegistered is true.
