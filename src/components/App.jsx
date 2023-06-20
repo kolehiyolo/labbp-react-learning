db843fcbb456e0576ea5c81b24eda8cccabcefab
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-20 15:08:41 +0800

    UI rendering current time on init and button change
    1. Used what I learned with useState to create the dynamic variable
    2. Button also works to update current time
    3. CSS is shot to hell and had to do some adjustments, will save on next commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+
+  function getCurrentTime() {
+    setCurrentTime(new Date().toLocaleTimeString());
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{currentTime}</h1>
+      <button onClick={getCurrentTime}>Get Time</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./components/App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+  <App />
 );
 
 //Challenge:
