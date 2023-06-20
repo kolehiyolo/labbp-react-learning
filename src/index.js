d6ef984972936a90a2a11e74e7d9225bc1103a0a
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-20 14:19:40 +0800

    Starting Files Ready
    1. Lesgawww

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from "./components/App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <h1>Hello World!</h1>
+    <App />
 );
+
+//Challenge:
+//1. Given that you can get the current time using:
+let time = new Date().toLocaleTimeString();
+console.log(time);
+//Show the latest time in the <h1> when the Get Time button
+//is pressed.
+
+//2. Given that you can get code to be called every second
+//using the setInterval method.
+//Can you get the time in your <h1> to update every second?
+
+//e.g. uncomment the code below to see Hey printed every second.
+// function sayHi() {
+//   console.log("Hey");
+// }
+// setInterval(sayHi, 1000);
