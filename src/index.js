1915495fdf82abb87b53f4cfb7493f251cd4381a
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-21 00:05:41 +0800

    Challenge done
    1. Learned about Destructuring via the MDN docs
    2. Got really bored and lost for a while since there are quite some Javascript-specific jargon thrown about, and most importantly, Javascript secret jutsus that I don't ever even really need, but probably will have to learn to be more of an elite Javascripter
    	2.1. Let's be real, though, a lot of these hidden jutsus (even the new batch of awesome stuff with ES6), aren't actually necessary at all to code and can be skipped entirely and are just there for quality-of-life for the devs, and even then it can be argued that they can muddy the waters for reading code
    	2.2. Perhaps I just need a bit more practice to really read and write ES6 comfortably, but I'm familiar enough that if thrown in the trenches of a heavily ES6 or hidden-jutsu-filled codebase, I'd be able to navigate it and even alter it given time and some healthy amounts of docs-checking and stack-overflowing
    3. My code works now after quite some trial and error and I have a working understanding of descructuring based on the example, but I'll still watch Angela's lesson as perhaps she has another one of those absurdly awesomely helpful perspectives and analogies for the concepts, AND possibly even a hidden jutsu or two

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import cars from "./practice.js";
+
+const [tesla, honda] = cars;
+
+const [
+  {
+    coloursByPopularity: [teslaTopColour],
+    speedStats: {
+      topSpeed: teslaTopSpeed
+    },
+  },
+  {
+    coloursByPopularity: [hondaTopColour],
+    speedStats: {
+      topSpeed: hondaTopSpeed
+    },
+  }
+ ] = cars;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <h1>Hello World!</h1>
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>
 );
