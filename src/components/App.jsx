841e8a4521f6cb634a2cffa61bffff4382146897
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-22 01:40:03 +0800

    Pause for today
    1. Taught Kren the fundamentals of HTML+CSS and that took the entire night
    2. Continuing tomorrow

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState();
+  const [name, setName] = useState();
+
+  function handleOnChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  function handleOnClick() {
+    setName(inputValue);
+  }
+
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {name}</h1>
+      <input 
+        onChange={handleOnChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={handleOnClick}>Submit</button>
     </div>
   );
 }
