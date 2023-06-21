3a5e493a3e7611ce0b4540d5a1f91a0c523ffa86
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-21 16:34:55 +0800

    Challenge Done
    1. Angela mentioned HTML Events, which is pretty solid and I look forward to checking the page out
    2. The ones used primarily for this lesson are onclick, onmouseover and onmouseout
    3. The solution I have is pretty janky, but it's the first solution that came to mind
    4. I'd like to see how Angela does her own thing and I might follow and commit hers if it's different and awesome enough

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState('Hello');
+  const [buttonStyle, setButtonStyle] = useState();
+
+  function handleClick() {
+    console.log(`Clicked`); 
+    const username = document.getElementById("username").value;
+
+    setHeadingText(`Hello, ${username}`);
+  }
+
+  function handleOnMouseOver() {
+    console.log(`handleOnMouseOver()`); 
+    setButtonStyle({background: "black", color: "white"});
+  }
+  
+  function handleOnMouseOut() {
+    console.log(`handleOnMouseOut()`); 
+    setButtonStyle();
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>{headingText}</h1>
+      <input id="username" type="text" placeholder="What's your name?" />
+      <button 
+        onClick={handleClick}
+        onMouseOver={handleOnMouseOver}
+        onMouseOut={handleOnMouseOut}
+        style={buttonStyle}
+      >Submit</button>
     </div>
   );
 }
