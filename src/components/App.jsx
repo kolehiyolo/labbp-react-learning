a9d4a7965123920e648a908231741864261f9a6d
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-24 00:29:55 +0800

    Starting Files Ready
    1. Lesgaw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 function App() {
   return (
-    <div></div>
+    <div className="container">
+      <h1>Hello</h1>
+      <form>
+        <input name="fName" placeholder="First Name" />
+        <input name="lName" placeholder="Last Name" />
+        <button>Submit</button>
+      </form>
+    </div>
   );
 }
 
