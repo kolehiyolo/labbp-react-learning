02a7abe2ef81ea73207cd9123453262e3f0a30a3
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-24 04:36:57 +0800

    Returned to default
    1. Apparently I didn't do a separate branch for lesson-405
    2. Remedied and reset this branch to the default starter state for the upcoming lessons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,8 @@
 import React, {useState} from "react";
-import Item from "./Item.jsx";
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [todoArray, setTodoArray] = useState([]);
-
   return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <form className="form">
-        <input onChange={(event) => {setInputValue(event.target.value)}} type="text" value={inputValue} />
-        <button 
-          onClick={(event) => {
-            setTodoArray((prevValue) => [...prevValue, inputValue]);
-            setInputValue('');
-            event.preventDefault();
-          }}
-          type="submit"
-        >
-          <span>Add</span>
-        </button>
-      </form>
-      <div>
-        <ul>
-          {todoArray.map((listItem, index) => <Item key={index} value={listItem}/>)};
-        </ul>
-      </div>
-    </div>
+    <div></div>
   );
 }
 
--- src/components/Item.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Item(props) {
-  console.log(`CALLED`); 
-  return <li>{props.value}</li>;
-}
-
-export default Item;
