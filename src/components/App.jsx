33cee879ba55664dca82d09c290aa8db1aefb1ab
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-24 03:51:25 +0800

    Item.jsx prepared
    1. Prepared a separate file for the list-item component and imported/exported to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Item from "./Item.jsx";
 
 function App() {
   return (
---
+++ src/components/Item.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Item() {
+  return <li></li>;
+}
+
+export default Item;
