2b97f8189cf6d78fc689d749012af002d1d76405
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-24 03:54:10 +0800

    Form ready and tested for submission
    1. Updated the div to be a proper form element instead and tested the non-refresh posting
    2. addTodoItem() ready but not built yet to actually save the input value

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import Item from "./Item.jsx";
 
 function App() {
+  function addTodoItem(event) {
+    console.log(`Add button pressed`); 
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
+      <form className="form">
         <input type="text" />
-        <button>
+        <button onClick={addTodoItem} type="submit">
           <span>Add</span>
         </button>
-      </div>
+      </form>
       <div>
         <ul>
           <li>A Item</li>
