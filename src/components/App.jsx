fa637243689bf61a8d2de6a2f5759b0f3975b406
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-24 00:40:14 +0800

    1st Challenge Done
    1. Seems like a very simple challenge
    2. All that I had to do was use to different state variables each bound to the changing of each input field
    3. When change is triggered, the values of the state variables are updated to reflect the respective input's value
    4. These state variables are shown then on the <h1> message
    	4.1. I tweaked it a tad so that the comma part will only show if there is either a first or last name present that is not null and I used the && operator learned a while ago to evaluate the expression to the right-hand side value IF the left hand side value is true

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+  const [fName, setFName] = useState('');
+  const [lName, setLName] = useState('');
+
+  function handleFNameInputChange(event) {
+    console.log(`handleFNameInputChange()`); 
+    setFName(event.target.value);
+  }
+  
+  function handleLNameInputChange(event) {
+    console.log(`handleLNameInputChange()`); 
+    setLName(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello{(fName || lName) && ', ' + fName + ' ' + lName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input onChange={handleFNameInputChange} name="fName" placeholder="First Name" />
+        <input onChange={handleLNameInputChange} name="lName" placeholder="Last Name" />
         <button>Submit</button>
       </form>
     </div>
