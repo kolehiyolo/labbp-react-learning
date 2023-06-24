1e380bd0f388a8e23b981fb02b11a2ab1e17fa55
Tristan Sean Paul Perfecto Cinco <71287768+kolehiyolo@users.noreply.github.com>
2023-06-24 21:16:39 +0800

    Challenge done
    1. It's a really really cool challenge that uses a lot of what I learned
    2. What I did is add an onClick trigger to the text area, which then sets a new state variable (uiExpanded) to true from false
    	2.1. This no longers turns back to falseas it requires clicking of the textarea which can be triggered if a user wants to actually type in
    3. I separated the Input and AddButton to their own components, but didn't move them to separate files and instead they're still in the same file
    4. A lot of the components then depend on this uiExpanded state to be true to render/change
    	4.1. Input's visibility is true if uiExpanded is true
    	4.2. AddButton's visibility is true if uiExpanded is true
    	4.3. The textarea's rows attribute is 3 if uiExpanded is true, 1 if false

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -3,11 +3,40 @@ import AddIcon from '@mui/icons-material/Add';
 import Fab from '@mui/material/Fab';
 import Zoom from '@mui/material/Zoom';
 
+function Input(props) {
+  return (
+    <Zoom in={true}>
+      <input
+        name="title"
+        onChange={props.handleChange}
+        value={props.note.title}
+        placeholder="Title"
+      />
+    </Zoom>
+  );
+}
+
+function AddButton(props) {
+  return (
+    <Zoom in={true}>
+      <Fab 
+        color="primary"
+        aria-label="add"
+        onClick={props.submitNote}
+      >
+        <AddIcon />
+      </Fab>
+    </Zoom>
+  )
+}
+
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+
+  const [uiExpanded, setUiExpanded] = useState(false);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -29,31 +58,34 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function expandTextArea() {
+    setUiExpanded(true);
+  }
+
   return (
     <div>
       <form className="create-note">
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
+        {
+          uiExpanded && 
+          <Input 
+            handleChange={handleChange}
+            note={note}
+          />
+        }
         <textarea
           name="content"
           onChange={handleChange}
+          onClick={expandTextArea}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={uiExpanded ? 3 : 1}
         />
-        <Zoom in={true}>
-          <Fab 
-            color="primary"
-            aria-label="add"
-            onClick={submitNote}
-          >
-            <AddIcon />
-          </Fab>
-        </Zoom>
+        {
+          uiExpanded && 
+          <AddButton 
+            submitNote={submitNote}
+          />
+        }
       </form>
     </div>
   );
